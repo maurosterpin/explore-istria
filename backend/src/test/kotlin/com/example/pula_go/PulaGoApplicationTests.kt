58dc5cd6a79b52a305bd0feaef69fc7c36787a75
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2025-01-13 17:07:02 +0100

    Add backend

---
+++ backend/src/test/kotlin/com/example/pula_go/PulaGoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pula_go
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PulaGoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
